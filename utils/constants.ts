95d4928c006f632164445073e6eb79ad5a287ba8
XY <39858937+xyftw@users.noreply.github.com>
2024-07-23 23:16:39 +0800

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,7 +3,7 @@ export const CHECKOUT_URL =
   "https://coolapps.lemonsqueezy.com/checkout/buy/f53d875a-4424-4ea3-8398-f3559dfaef98";
 export const ENABLE_SHOP = process.env.NEXT_PUBLIC_ENABLE_SHOP === "true";
 export const DEFAULT_PROMPT =
-  "你是一个专业的翻译。请逐行翻译下面的文本到{{target_lang}}，注意保留数字和换行符，请勿自行创建内容，除了翻译，不要输出任何其他文本。";
+  "You are a highly skilled translation engine with expertise in the technology sector. Your function is to translate texts accurately into the target {{target_lang}}, maintaining the original format, technical terms, and abbreviations. Do not add any explanations or annotations to the translated text.";
 export const DEFAULT_BASE_URL_HOST = "https://api.openai.com";
 
 //cache key
@@ -34,7 +34,7 @@ export const getCustomConfigCache = (): CustomConfigItem => {
   return {
     customModel: "gpt-3.5-turbo-16k",
     promptTemplate:
-      "你是一个专业的翻译。请逐行翻译下面的文本到{{target_lang}}，注意保留数字和换行符，请勿自行创建内容，除了翻译，不要输出任何其他文本。",
+      "You are a highly skilled translation engine with expertise in the technology sector. Your function is to translate texts accurately into the target {{target_lang}}, maintaining the original format, technical terms, and abbreviations. Do not add any explanations or annotations to the translated text.",
   } as CustomConfigItem;
 };
 
